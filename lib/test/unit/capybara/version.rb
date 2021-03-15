5e9eb95f6fd927b79793fb000b0570d7f6a5304f
Sutou Kouhei <kou@clear-code.com>
2021-03-15 11:44:50 +0900

    Bump version

--- lib/test/unit/capybara/version.rb
+++ lib/test/unit/capybara/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2019  Sutou Kouhei <kou@clear-code.com>
+# Copyright (C) 2011-2021  Sutou Kouhei <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
 module Test
   module Unit
     module Capybara
-      VERSION = "1.1.0"
+      VERSION = "1.1.1"
     end
   end
 end
